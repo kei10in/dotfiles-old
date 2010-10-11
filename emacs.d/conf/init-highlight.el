c2f55e856147593cc43864de7a29b6e87728aadf
keisuke <kei10in@gmail.com>
2010-10-11 09:57:02 +0900

    add comment

--- emacs.d/conf/init-highlight.el
+++ emacs.d/conf/init-highlight.el
@@ -8,7 +8,15 @@
 (require 'font-lock)
 
 ;;; Highlight parentheses, braces, brackets.
+;;; You can use C-M-p or C-M-n to jump to corresponding paren
 (show-paren-mode t)
+;; カッコ対応表示のスタイル
+;; カッコその物に色が付く(デフォルト)
+;; (setq show-paren-style 'parenthesis)
+;; カッコ内に色が付く
+;; (setq show-paren-style 'expression)
+;; 画面内に収まる場合はカッコのみ、画面外に存在する場合はカッコ内全体に色が付く
+;; (setq show-paren-style 'mixed)
 
 ;;; Highlight region
 (setq transient-mark-mode nil)
