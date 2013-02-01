2b908ddea7796d42a845c6929dbd0a3fc851f4f7
kei10in <kei10in@gmail.com>
2013-02-01 16:50:13 +0900

    [Emacs] configurate scala-mode2

--- emacs.d/conf/init-scala.el
+++ emacs.d/conf/init-scala.el
@@ -32,6 +32,26 @@
 
 
 ;;; Scala-mode
+(require 'scala-mode2)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(scala-indent:align-parameters t)
+ '(scala-indent:indent-value-expression t)
+)
+
+(add-hook
+ 'scala-mode-hook
+ '(lambda ()
+    (local-set-key (kbd "RET")
+                   'newline-and-indent)
+    (local-set-key (kbd "<backtab>")
+                   'scala-indent:indent-with-reluctant-strategy)
+    ))
+
 
 (provide 'init-scala)
 ;;; init-scala.el ends here
