968faec46ac402142a6bca692a05942fdb282274
kei10in <kei10in@gmail.com>
2013-03-25 08:18:05 +0900

    [Emacs] fix scala indentation

--- emacs.d/conf/init-scala.el
+++ emacs.d/conf/init-scala.el
@@ -39,8 +39,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(scala-indent:align-parameters t)
- '(scala-indent:indent-value-expression t)
+ ;; '(scala-indent:align-parameters t)
+ ;; '(scala-indent:indent-value-expression t)
 )
 
 (require 'utils)
